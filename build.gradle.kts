1229aa28228190072cac648756ee9b4a7e5ba1f1
Ken Stanley <dohpaz@gmail.com>
2026-01-21 14:46:13 -0500

    Update to use IntelliJ Platform Gradle Plugin (2.x)﻿

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+// Based on intellij-platform-plugin-template (https://github.com/JetBrains/intellij-platform-plugin-template/blob/de6b4c8a9db474d10d00c7cd26fc2f280a057ed3/build.gradle.kts)
+
+plugins {
+    id("java")
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.intelliJPlatform)
+}
+
+val htmlFixer = { fileName: String -> File(fileName).readText().replace("<html>", "").replace("</html>", "") }
+
+group = providers.gradleProperty("pluginGroup").get()
+version = providers.gradleProperty("pluginVersion").get()
+
+kotlin {
+    jvmToolchain(21)
+}
+
+repositories {
+    mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdea(providers.gradleProperty("platformVersion"))
+
+        plugins(providers.gradleProperty("platformPlugins").map { it.split(",") })
+
+        pluginVerifier()
+        zipSigner()
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        name = providers.gradleProperty("pluginName")
+        version = providers.gradleProperty("pluginVersion")
+
+        description = htmlFixer("src/main/resources/META-INF/description.html")
+        changeNotes = htmlFixer("src/main/resources/META-INF/change-notes.html")
+
+        ideaVersion {
+            sinceBuild = providers.gradleProperty("pluginSinceBuild")
+        }
+    }
+
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
+}
+
+tasks {
+    wrapper {
+        gradleVersion = providers.gradleProperty("gradleVersion").get()
+    }
+}
